c36c5145171d9964fcbc687f2f36706fd4fa6256
Roel van Bakel <roel.vanbakel@cpp.canon>
2023-09-04 15:51:47 +0200

    Small typo

--- src-test/multi-language.test.ts
+++ src-test/multi-language.test.ts
@@ -2,7 +2,7 @@ import {assertEquals} from "./deps.ts"
 import {undefinedValuesDeletedFrom} from "./utils/test-helpers.ts"
 import {serializeNodes} from "../src/serializer.ts"
 import {deserializeModel} from "../src/deserializer.ts"
-import {libraryModel, libraryModelApi} from "./library.ts"
+import {libraryModel} from "./library.ts"
 import {multiLanguage} from "./m3/multi-language.ts"
 import {multiModel, multiModelApi} from "./multi.ts"
 
@@ -11,7 +11,7 @@ Deno.test("Library test model", async (tctx) => {
 
     await tctx.step("[de-]serialize multi-language model", () => {
         const serialization = serializeNodes(multiModel, multiModelApi)
-        const deserialization = deserializeModel(undefinedValuesDeletedFrom(serialization), libraryModelApi, multiLanguage, [])
+        const deserialization = deserializeModel(undefinedValuesDeletedFrom(serialization), multiModelApi, multiLanguage, [])
         assertEquals(deserialization, libraryModel)
     })
 })
