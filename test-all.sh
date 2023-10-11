90bc5501ffd1e1b6e2c2dcda7d988e9e5bc88100
Meinte Boersma <meinte.boersma@gmail.com>
2023-10-11 10:56:03 +0200

    GitHub Action doesn't know pushd/popd...

--- test-all.sh
+++ test-all.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env sh
 
+cd packages
+
 # none of these should break/crash!:
 
-pushd packages/test && npm test && popd
-pushd packages/artifacts && npm run generate && popd
-pushd packages/cli && node dist/lionweb-cli.js && popd
-pushd packages/validation && npm test && popd
+cd test && npm test && cd ..
+cd artifacts && npm run generate && cd ..
+cd cli && node dist/lionweb-cli.js && cd ..
+cd validation && npm test && cd ..
+
+cd ..
 
