90bdbe2195ba409a3e79efe24431344030c0bb9d
Meinte Boersma <meinte.boersma@gmail.com>
2023-10-13 09:32:45 +0200

    add a command to the CLI tool

--- packages/cli/src/lionweb-cli.ts
+++ packages/cli/src/lionweb-cli.ts
@@ -2,6 +2,7 @@ import {argv} from "process"
 
 import {extractFromSerialization} from "./serialization-extractor.js"
 import {diagramFromSerialization} from "./m3/diagram-generator.js"
+import {generateTsTypesWith} from "./m3/ts-types-generator.js"
 
 
 const main = (args: string[])=> {
@@ -19,14 +20,18 @@ Available commands are:
         return
     }
 
+    const DIAGRAM_COMMAND = "diagram"
+    const EXTRACT_COMMAND = "extract"
+    const GENERATE_TS_TYPES_COMMAND = "generate-ts-types"
+
     const command = args[2]
     const commandArgs = args.slice(3)
     switch (command) {
 
-        case "diagram": {
+        case DIAGRAM_COMMAND: {
             if (commandArgs.length === 0) {
                 console.log(
-                    `The diagram command generates a PlantUML and Mermaid diagram for the language that the given paths point to.`
+                    `The ${DIAGRAM_COMMAND} command generates a PlantUML and Mermaid diagram for the language that the given paths point to.`
                 )
             } else {
                 commandArgs.forEach(diagramFromSerialization)
@@ -34,14 +39,25 @@ Available commands are:
             return
         }
 
-        case "extract": {
+        case EXTRACT_COMMAND: {
             if (commandArgs.length === 0) {
                 console.log(
-`The extract command extracts the following from a serialization chunk in the form of files: a sorted JSON, and a shortened JSON.
+`The ${EXTRACT_COMMAND} command extracts the following from a serialization chunk in the form of files: a sorted JSON, and a shortened JSON.
 If the chunk is the serialization of a LionCore Language/M2, then a textual rendering is already output.`
                 )
             } else {
                 commandArgs.forEach(extractFromSerialization)
+            }
+            return
+        }
+
+        case GENERATE_TS_TYPES_COMMAND: {
+            if (commandArgs.length === 0) {
+                console.log(
+`The ${GENERATE_TS_TYPES_COMMAND} command generates a TypeScript source files with type definitions for the given (JSON serializations of) languages, assuming the use of the dynamic façade.`
+                )
+            } else {
+                generateTsTypesWith(commandArgs)
             }
             return
         }
