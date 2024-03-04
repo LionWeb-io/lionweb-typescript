96c897c891871819bafd4d76c7eab0dcba664d67
indamutsa <arsichizy@gmail.com>
2024-03-04 13:00:56 +0100

    Organizing measure.ts

--- packages/cli/src/serialization/measurer.ts
+++ packages/cli/src/serialization/measurer.ts
@@ -1,35 +1,32 @@
-import {extname} from "path"
-import {Language, MetaPointer, SerializationChunk, groupBy} from "@lionweb/core"
-import {readChunk, tryLoadAllAsLanguages, writeJsonAsFile} from "@lionweb/utilities"
-import {separate} from "../language-aware-args.js"
-
+import { extname } from "path"
+import { Language, MetaPointer, SerializationChunk, groupBy, MemoisingSymbolTable } from "@lionweb/core"
+import { readChunk, tryLoadAllAsLanguages, writeJsonAsFile } from "@lionweb/utilities"
+import { separate } from "../language-aware-args.js"
 
 export const executeMeasureCommand = async (args: string[]) => {
-    const {chunkPaths, languagePaths} = separate(args)
+    const { chunkPaths, languagePaths } = separate(args)
     const languages = await tryLoadAllAsLanguages(languagePaths)
-    chunkPaths.forEach((chunkPath) => {
+    chunkPaths.forEach(chunkPath => {
         measureSerializationChunk(chunkPath, languages)
     })
 }
 
 const measureSerializationChunk = async (path: string, languages: Language[]) => {
+    // TODO - Pass in languages to measure function
+
     const chunk = await readChunk(path)
     const extLessPath = path.substring(0, path.length - extname(path).length)
-    const metricsPath = extLessPath + ".metrics.json"   
-    writeJsonAsFile(
-        metricsPath,
-        measure(chunk)
-    )
+    const metricsPath = extLessPath + ".metrics.json"
+    writeJsonAsFile(metricsPath, measure(chunk, languages))
     console.log(`Wrote metrics for ${path} --> ${metricsPath}`)
 }
-
 
 // TODO  move all of this to @lionweb/utilities (in the end)
 
 type ClassifierInstantiationMetric = {
     name?: string
-    key: string         // == key of classifier
-    language: string    // == key of language
+    key: string // == key of classifier
+    language: string // == key of language
     version: string
     count: number
     // TODO  add property to say the classifier is a concept, or annotation (or enum)?
@@ -39,14 +36,21 @@ type Metrics = {
     instantiations: ClassifierInstantiationMetric[]
 }
 
-const measure = (serializationChunk: SerializationChunk): Metrics => {
-        // Group nodes by classifier key, language, and version
-        const instantiationsPerId = groupBy(serializationChunk.nodes, ({ classifier }) => `${classifier.key}:${classifier.language}:${classifier.version}`)
+const measure = (serializationChunk: SerializationChunk, languages: Language[]): Metrics => {
+    const symbolTable = new MemoisingSymbolTable(languages)
 
-        // Map grouped nodes to instantiations with count
-        const instantiations = Object.values(instantiationsPerId).map((nodes) => 
-             ({ ...nodes[0].classifier, count: nodes.length })
-        )
-        // Return the metrics object with instantiations
-        return { instantiations }
+    // Group nodes by classifier key, language, and version
+    const instantiationsPerId = groupBy(
+        serializationChunk.nodes,
+        ({ classifier }) => `${classifier.key}:${classifier.language}:${classifier.version}`
+    )
+
+    // Map grouped nodes to instantiations with count
+    const instantiations = Object.values(instantiationsPerId).map(nodes => ({
+        ...nodes[0].classifier,
+        name: symbolTable.entityMatching(nodes[0].classifier)?.name,
+        count: nodes.length
+    }))
+    // Return the metrics object with instantiations
+    return { instantiations }
 }
