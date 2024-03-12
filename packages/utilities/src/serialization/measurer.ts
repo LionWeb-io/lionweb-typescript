43a66c8198291c5842a66a717c017af2ec808f73
Meinte Boersma <meinte.boersma@gmail.com>
2024-03-12 10:20:46 +0100

    switch over to use nested maps/dictionaries throughout

--- packages/utilities/src/serialization/measurer.ts
+++ packages/utilities/src/serialization/measurer.ts
@@ -11,6 +11,13 @@ import { Metrics } from "./metric-types.js"
 
 
 /**
+ * Sum the given array of numbers.
+ */
+const sumNumbers = (nums: number[]): number =>
+    nums.reduce((acc, cur) => acc + cur, 0)
+
+
+/**
  * Computes {@link Metrics metrics} on the given {@link SerializationChunk serializationChunk}.
  * Passing it {@link Language languages} make this language-aware:
  *  * classifier names are looked up,
@@ -19,62 +26,72 @@ import { Metrics } from "./metric-types.js"
 export const measure = (serializationChunk: SerializationChunk, languages: Language[]): Metrics => {
     const symbolTable = new MemoisingSymbolTable(languages)
 
-    const metaPointerAsText = (classifier: MetaPointer) => `${classifier.key}:${classifier.language}:${classifier.version}`
-
-    // group nodes by classifier key, language, and version:
-    const instantiationsPerMetaPointer = groupBy(serializationChunk.nodes, ({ classifier }) => metaPointerAsText(classifier))
-
-    // map grouped nodes to instantiations with count:
-    const instantiations = Object.values(instantiationsPerMetaPointer).map((nodes) => {
-        const classifier = nodes[0].classifier
-        const language = symbolTable.languageMatching(classifier.language, classifier.version)
-        return {
-            language: {
-                key: classifier.language,
-                version: classifier.version,
-                name: language?.name
-            },
-            key: classifier.key,
-            name: symbolTable.entityMatching(nodes[0].classifier)?.name,
-            instantiations: nodes.length
-        }
-    })
-
-    // compute all concrete concepts for the language:
-    const concreteConcepts = languages.flatMap(conceptsOf).filter((concept) => !concept.abstract)
-    const unusedConcreteConcepts = concreteConcepts
-        .map((concept) => concept.metaPointer())
-        .filter((metaPointer) => !(metaPointerAsText(metaPointer) in instantiationsPerMetaPointer))
-        .map((metaPointer) => ({
-            language: {
-                key: metaPointer.language,
-                version: metaPointer.version,
-                name: symbolTable.languageMatching(metaPointer.language, metaPointer.version)?.name
-            },
-            key: metaPointer.key,
-            name: symbolTable.entityMatching(metaPointer)?.name
-        }))
-
-    const languageKey2version2instantiations =
+    // group nodes by language key, version, and classifier key, mapped to the classifier meta-pointer and #instantiations:
+    const languageKey2version2classifierKey2info =
         mapValues(
             groupBy(serializationChunk.nodes, ({ classifier }) => classifier.language),
             (nodes) => mapValues(
-                groupBy(nodes, ({ classifier  }) => classifier.version),
-                (nodes) => nodes.length
+                groupBy(nodes, ({ classifier }) => classifier.version),
+                (nodes) => mapValues(
+                    groupBy(nodes, ({ classifier }) => classifier.key),
+                    (nodes) => ({ classifier: nodes[0].classifier, instantiations: nodes.length })
+                )
             )
         )
-    const usedLanguages = Object.entries(languageKey2version2instantiations)
-        .flatMap(([key, version2Count]) =>
-            Object.entries(version2Count)
-                .map(([version, instantiations]) => ({
-                    key,
-                    version,
-                    name: symbolTable.languageMatching(key, version)?.name,
-                    instantiations
-                }))
-        )
 
-    // return the metrics object:
+    // map grouped nodes to info including #instantiations:
+    const instantiations =
+        Object.entries(languageKey2version2classifierKey2info)
+            .flatMap(([languageKey, version2classifierKey2info]) =>
+                Object.entries(version2classifierKey2info)
+                    .flatMap(([version, classifierKey2info]) =>
+                        Object.entries(classifierKey2info)
+                            .map(([classifierKey, info]) => ({
+                                language: {
+                                    key: languageKey,
+                                    version,
+                                    name: symbolTable.languageMatching(languageKey, version)?.name
+                                },
+                                key: classifierKey,
+                                name: symbolTable.entityMatching(info.classifier)?.name,
+                                instantiations: info.instantiations
+                            }))
+                    )
+            )
+
+    const isClassifierUsed = (metaPointer: MetaPointer): boolean =>
+           metaPointer.language in languageKey2version2classifierKey2info
+        && metaPointer.version in languageKey2version2classifierKey2info[metaPointer.language]
+        && metaPointer.key in languageKey2version2classifierKey2info[metaPointer.language][metaPointer.version]
+
+    const concreteConcepts = languages.flatMap(conceptsOf).filter((concept) => !concept.abstract)
+
+    const unusedConcreteConcepts =
+        concreteConcepts
+            .map((concept) => concept.metaPointer())
+            .filter((metaPointer) => !isClassifierUsed(metaPointer))
+            .map((metaPointer) => ({
+                language: {
+                    key: metaPointer.language,
+                    version: metaPointer.version,
+                    name: symbolTable.languageMatching(metaPointer.language, metaPointer.version)?.name
+                },
+                key: metaPointer.key,
+                name: symbolTable.entityMatching(metaPointer)?.name
+            }))
+
+    const usedLanguages =
+        Object.entries(languageKey2version2classifierKey2info)
+            .flatMap(([languageKey, version2classifierKey2info]) =>
+                Object.entries(version2classifierKey2info)
+                    .flatMap(([version, classifierKey2info]) => ({
+                        key: languageKey,
+                        version,
+                        name: symbolTable.languageMatching(languageKey, version)?.name,
+                        instantiations: sumNumbers(Object.values(classifierKey2info).map((info) => info.instantiations))
+                    }))
+            )
+
     return {
         usedLanguages,
         instantiations,
