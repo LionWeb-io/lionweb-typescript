7a8f6cf422288d739c3b90cf322fb14ec4df83f0
Ahmed Negm <ahmed.negm@mendix.com>
2024-03-24 14:31:28 +0100

    Generate languages in the same directory

--- packages/cli/src/inferer.ts
+++ packages/cli/src/inferer.ts
@@ -1,12 +1,16 @@
 import fs from "fs"
+import path from "path"
 import { serializeLanguages } from "@lionweb/core"
 import { inferLanguagesFromChunk, readChunk } from "@lionweb/utilities"
 
 export async function inferLanguages(filePath: string) {
+    const dirName = path.dirname(filePath)
     const chunk = await readChunk(filePath)
 
     const languages = inferLanguagesFromChunk(chunk)
     for (const language of languages) {
-        fs.writeFileSync(`${language.name}.json`, JSON.stringify(serializeLanguages(language), null, 2))
+        const languageFile = path.join(dirName, `${language.name}-Language.json`)
+        fs.writeFileSync(languageFile, JSON.stringify(serializeLanguages(language), null, 2))
+        console.log(`Language ${language.name} has been generated: "${languageFile}"`)
     }
 }
