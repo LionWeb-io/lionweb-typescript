7e6e2ba05b68ce30ad115a30dd959881290c53ca
Ahmed Negm <negmsoftware@gmail.com>
2024-03-27 15:55:26 +0100

    #137 Infer (partial) language(s) definition from a given serialization chunk (#141)
    * Infer languages from a given serialization chunk
    
    * Check the cardinality of the containment
    
    * Infer the property type from the value
    
    * Generate languages in the same directory
    
    * Try various key separators
    
    * Add unit tests
    
    * Add unit tests for `deriveLikelyPropertyName`

---
+++ packages/test/src/languages/minimal-library.ts
@@ -0,0 +1,27 @@
+import { builtinPrimitives, chain, concatenator, LanguageFactory, lastOf } from "@lionweb/core"
+import { hasher } from "@lionweb/utilities"
+
+const factory = new LanguageFactory("library", "1", chain(concatenator("-"), hasher()), lastOf)
+export const minimalLibraryLanguage = factory.language
+
+const { integerDatatype, stringDatatype } = builtinPrimitives
+
+const library = factory.concept("Library", false)
+const book = factory.concept("Book", false)
+const guideBookWriter = factory.concept("GuideBookWriter", false)
+
+const library_name = factory.property(library, "name").ofType(stringDatatype).havingKey("library_Library_name")
+const library_books = factory.containment(library, "books").ofType(book).isMultiple()
+library.havingFeatures(library_name, library_books)
+
+const book_title = factory.property(book, "title").ofType(stringDatatype)
+const book_pages = factory.property(book, "pages").ofType(integerDatatype)
+const book_type = factory.property(book, "type").ofType(stringDatatype)
+const book_author = factory.reference(book, "author").ofType(guideBookWriter)
+book.havingFeatures(book_title, book_pages, book_type, book_author)
+
+const writer_name = factory.property(guideBookWriter, "name").ofType(stringDatatype).havingKey("library_Writer_name")
+const guideBookWriter_countries = factory.property(guideBookWriter, "countries").ofType(stringDatatype)
+guideBookWriter.havingFeatures(guideBookWriter_countries, writer_name)
+
+minimalLibraryLanguage.havingEntities(book, library, guideBookWriter)
---
+++ packages/utilities/src/inferLanguages.ts
@@ -0,0 +1,150 @@
+import {
+    Concept,
+    Containment,
+    Language,
+    Link,
+    Property,
+    Reference,
+    SerializationChunk,
+    asArray,
+    builtinPrimitives,
+    chain,
+    concatenator,
+    lastOf
+} from "@lionweb/core"
+import { hasher } from "./hashing.js"
+
+const possibleKeySeparators = ["-", "_"]
+
+const id = chain(concatenator("-"), hasher())
+const key = lastOf
+
+const { stringDatatype, booleanDatatype, integerDatatype } = builtinPrimitives
+
+export function inferLanguagesFromChunk(chunk: SerializationChunk): Language[] {
+    const languages = new Map<string, Language>()
+    const concepts = new Map<string, Concept>()
+    const links = new Array<{ link: Link; conceptId: string }>()
+
+    for (const chunkLanguage of chunk.languages) {
+        const languageName = chunkLanguage.key
+        const language = new Language(languageName, chunkLanguage.version, id(languageName), key(languageName))
+        languages.set(languageName, language)
+    }
+
+    for (const node of chunk.nodes) {
+        const languageName = node.classifier.language
+        const entityName = node.classifier.key
+
+        const language = getLanguage(languages, languageName)
+        if (language.entities.filter(entity => entity.key === entityName).length) {
+            continue
+        }
+
+        const concept = new Concept(language, entityName, key(language.name, entityName), id(language.name, entityName), false)
+        language.havingEntities(concept)
+        concepts.set(node.id, concept)
+
+        for (const property of node.properties) {
+            const propertyName = deriveLikelyPropertyName(property.property.key)
+            if (concept.features.filter(feature => feature.key === propertyName).length) {
+                continue
+            }
+
+            const feature = new Property(
+                concept,
+                propertyName,
+                key(languageName, concept.name, propertyName),
+                id(languageName, concept.name, propertyName)
+            ).havingKey(property.property.key)
+
+            if (isBoolean(property.value)) {
+                feature.ofType(booleanDatatype)
+            } else if (isNumeric(property.value)) {
+                feature.ofType(integerDatatype)
+            } else {
+                feature.ofType(stringDatatype)
+            }
+
+            concept.havingFeatures(feature)
+        }
+
+        for (const containment of node.containments) {
+            const containmentName = containment.containment.key
+
+            if (concept.features.filter(feature => feature.key === containmentName).length) {
+                continue
+            }
+
+            const children = asArray(containment.children)
+            const feature = new Containment(
+                concept,
+                containmentName,
+                key(languageName, concept.name, containmentName),
+                id(languageName, concept.name, containmentName)
+            )
+            if (children.length) {
+                feature.isMultiple()
+            }
+            concept.havingFeatures(feature)
+
+            links.push({ link: feature, conceptId: children[0] })
+        }
+
+        for (const reference of node.references) {
+            const referenceName = reference.reference.key
+            if (concept.features.filter(feature => feature.key === referenceName).length) {
+                continue
+            }
+
+            const feature = new Reference(
+                concept,
+                referenceName,
+                key(languageName, concept.name, referenceName),
+                id(languageName, concept.name, referenceName)
+            )
+            concept.havingFeatures(feature)
+
+            const value = reference.targets[0].reference
+            links.push({ link: feature, conceptId: value })
+        }
+    }
+
+    for (const link of links) {
+        const linkedConcept = concepts.get(link.conceptId)
+        if (linkedConcept) {
+            link.link.ofType(linkedConcept)
+        } else {
+            // Containment of primitive types??
+        }
+    }
+
+    return Array.from(languages.values())
+}
+
+function getLanguage(languages: Map<string, Language>, languageName: string) {
+    const language = languages.get(languageName)
+    if (language === undefined) {
+        throw new Error(`Language '${languageName} is not exist in the languages section`)
+    }
+    return language
+}
+
+export function deriveLikelyPropertyName(key: string) {
+    for (const separator of possibleKeySeparators) {
+        const name = key.split(separator)[2]
+        if (name) {
+            return name
+        }
+    }
+
+    return key;
+}
+
+function isBoolean(value: string) {
+    return value === "true" || value === "false"
+}
+
+function isNumeric(value: string) {
+    return !isNaN(parseFloat(value))
+}
