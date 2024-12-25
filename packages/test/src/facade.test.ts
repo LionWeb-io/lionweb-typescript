7e4db44922be8f76c5bfecce5685947e3faf71ef
Meinte Boersma <meinte.boersma@gmail.com>
2024-12-25 23:31:31 +0100

    results of childrenExtractorUsing (and therefore: nodesExtractorUsing) functions now also return annotations

---
+++ packages/test/src/facade.test.ts
@@ -0,0 +1,61 @@
+import {childrenExtractorUsing, dynamicExtractionFacade, DynamicNode, Node, nodesExtractorUsing} from "@lionweb/core"
+
+import {assert} from "chai"
+const {deepEqual} = assert
+
+import {Annotated, Circle, Coord} from "./languages/shapes.js"
+
+
+const center = {
+    id: "center",
+    classifier: Coord,
+    settings: {
+        x: 1,
+        y: 2,
+        z: 3
+    },
+    annotations: [
+        {
+            id: "annotated",
+            classifier: Annotated,
+            settings: {},
+            annotations: []
+        }
+    ]
+};
+
+const circle: DynamicNode = {
+    id: "circle",
+    classifier: Circle,
+    settings: {
+        center,
+        radius: 5
+    },
+    annotations: []
+};
+
+
+describe("annotations are extracted", () => {
+
+    const idOf = ({ id }: Node) => id
+
+    it("by childrenExtractorUsing", () => {
+        deepEqual(
+            childrenExtractorUsing(dynamicExtractionFacade)(circle).map(idOf),
+            ["center"]
+        );
+        deepEqual(
+            childrenExtractorUsing(dynamicExtractionFacade)(center).map(idOf),
+            ["annotated"]
+        );
+    });
+
+    it("by nodesExtractorUsing", () => {
+        deepEqual(
+            nodesExtractorUsing(dynamicExtractionFacade)(circle).map(idOf),
+            ["circle", "center", "annotated"]
+        );
+    });
+
+});
+
