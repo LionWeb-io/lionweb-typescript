2357b786a187a5a4b6c451ff068a908f42754b4c
Meinte Boersma <meinte.boersma@gmail.com>
2025-06-27 16:00:07 +0200

    (fix file's license header + remove unnecessary shenanigans)

--- packages/class-core-generator/src/generation-headers.ts
+++ packages/class-core-generator/src/generation-headers.ts
@@ -1,15 +1,21 @@
-// SPDX-FileCopyrightText: 2025 TRUMPF Laser SE
+// Copyright 2025 TRUMPF Laser SE and other contributors
 //
-// SPDX-License-Identifier: LicenseRef-TRUMPF
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-FileCopyrightText: 2025 TRUMPF Laser SE and other contributors
+// SPDX-License-Identifier: Apache-2.0
 
 import { asString } from "littoral-templates"
-
-/**
- * @return the given string, reversed — "abc" -> "cba"
- * Note: only used to hack around the REUSE compliance checker!
- */
-const reverseString = (str: string) =>
-    str.split("").reverse().join("")
 
 /**
  * The license header (as //-styled comments) for code originating from Trumpf Laser SE, licensed under Apache-2.0.
@@ -28,8 +34,8 @@ const trumpfOriginatingApache2_0LicenseHeaderComments = `// Copyright 2025 TRUMP
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-// ${reverseString("txeTthgirypoCeliF-XDPS")}: 2025 TRUMPF Laser SE and other contributors
-// ${reverseString("reifitnedI-esneciL-XDPS")}: Apache-2.0`
+// SPDX-FileCopyrightText: 2025 TRUMPF Laser SE and other contributors
+// SPDX-License-Identifier: Apache-2.0`
 
 /**
  * Generic (//-styled) comments warning about code being generated.
