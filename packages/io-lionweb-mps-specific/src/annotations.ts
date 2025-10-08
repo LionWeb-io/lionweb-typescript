7632ebe53880682c79ea52844866457dbfd21263
Meinte Boersma <meinte.boersma@gmail.com>
2025-10-08 13:36:59 +0200

    move 'repairIoLionWebMpsSpecificAnnotations' function from elsewhere to io-lionweb-mps-specific package

--- packages/io-lionweb-mps-specific/src/annotations.ts
+++ packages/io-lionweb-mps-specific/src/annotations.ts
@@ -16,7 +16,11 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { M3Concept } from "@lionweb/core"
+import { LionWebJsonChunk, LionWebJsonMetaPointer } from "@lionweb/json"
+
 import { IoLionWebMpsSpecificAnnotation } from "./implementation.js"
+import { ioLionWebMpsSpecificLanguage } from "./definition.js"
+
 
 /**
  * @return the `io.lionweb.mps.specific` annotations present on the given node.
@@ -25,4 +29,33 @@ export const ioLionWebMpsSpecificAnnotationsFrom = (node: M3Concept): IoLionWebM
     node
         .annotations
         .filter((annotation) => annotation instanceof IoLionWebMpsSpecificAnnotation) as IoLionWebMpsSpecificAnnotation[]
+
+
+/**
+ * Replaces stated version "0" of language "io-lionweb-mps-specific" with its actual version.
+ */
+export const repairIoLionWebMpsSpecificAnnotations = (serializationChunk: LionWebJsonChunk) => {
+    serializationChunk.languages.forEach((usedLanguage) => {
+        if (usedLanguage.key === ioLionWebMpsSpecificLanguage.key) {
+            usedLanguage.version = ioLionWebMpsSpecificLanguage.version
+        }
+    })
+    const repairMetaPointer = (metaPointer: LionWebJsonMetaPointer): void => {
+        if (metaPointer.language === ioLionWebMpsSpecificLanguage.key) {
+            metaPointer.version = ioLionWebMpsSpecificLanguage.version
+        }
+    }
+    serializationChunk.nodes.forEach((node) => {
+        repairMetaPointer(node.classifier)
+        node.properties.forEach((propertySetting) => {
+            repairMetaPointer(propertySetting.property)
+        })
+        node.containments.forEach((containmentSetting) => {
+            repairMetaPointer(containmentSetting.containment)
+        })
+        node.references.forEach((referenceSetting) => {
+            repairMetaPointer(referenceSetting.reference)
+        })
+    })
+}
 
