7a5a4c6aec372403f100ea4288be92686a443a56
Jos Warmer <jos.warmer@openmodeling.nl>
2025-11-02 13:42:10 +0100

    Refactor validation to support delta messages

--- packages/validation/src/validators/definitions/DeltaTypesDefinitions.ts
+++ packages/validation/src/validators/definitions/DeltaTypesDefinitions.ts
@@ -31,12 +31,8 @@ export const DeltaTypesDefinitions: TypeGroup = {
             name: "Boolean",
             primitiveType: "boolean",
         },
-        {
-            name: "ClientId",
-            primitiveType: "string",
-        },
     ],
-    structuredTypes: [
+    objectTypes: [
         {
             name: "ProtocolMessage",
             properties: [
@@ -103,15 +99,7 @@ export const DeltaTypesDefinitions: TypeGroup = {
         },
         {
             name: "LionWebDeltaJsonChunk",
-            properties: [
-                {
-                    name: "nodes",
-                    type: "LionWebJsonNode",
-                    isList: true,
-                    isOptional: false,
-                    mayBeNull: false,
-                },
-            ],
+            properties: [],
         },
     ],
 };
