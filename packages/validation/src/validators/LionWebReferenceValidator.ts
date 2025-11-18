10b9badba988e8845fd893a37504cc1152a52b69
Jos Warmer <jos.warmer@openmodeling.nl>
2025-11-18 11:24:32 +0100

    Fix typos

--- packages/validation/src/validators/LionWebReferenceValidator.ts
+++ packages/validation/src/validators/LionWebReferenceValidator.ts
@@ -23,13 +23,14 @@ export class LionWebReferenceValidator {
     }
 
     /**
-     * Checks for duplicate node id's.
-     * As a side-effect creates a map from `id` to `node` for all nodes in `chunk` to make further validation faster.
+     * Vhecks for dup;licate node id's.
+     * As a side-effect creates a map from `id` to `node` to make further validation faster.
+     * @param obj
      * @param ctx
      */
-    validateNodeIds(chunk: LionWebJsonChunk, ctx: JsonContext): void {
+    validateNodeIds(obj: LionWebJsonChunk, ctx: JsonContext): void {
         // put all nodes in a map, validate that there are no two nodes with the same id.
-        chunk.nodes.forEach((node, index) => {
+        obj.nodes.forEach((node, index) => {
             // this.validationResult.check(this.nodesIdMap.get(node.id) === undefined, `Node number ${index} has duplicate id "${node.id}"`);
             if (!(this.nodesIdMap.get(node.id) === undefined)) {
                 this.validationResult.issue(new Reference_DuplicateNodeId_Issue(ctx.concat("nodes", index), node.id))
