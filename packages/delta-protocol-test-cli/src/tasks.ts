902cbea3ee2eec822078c7e83c5a59876b0fa9de
Meinte Boersma <meinte.boersma@gmail.com>
2025-12-09 11:06:06 +0100

    align semantics of TS delta test CLI client with those of the C# implementation

--- packages/delta-protocol-test-cli/src/tasks.ts
+++ packages/delta-protocol-test-cli/src/tasks.ts
@@ -109,105 +109,146 @@ export const taskExecutor = (lionWebClient: LionWebClient, semanticLogItems: ISe
 
     const linkTestConcept = (id?: LionWebId) =>
         id === undefined
-            ? thePartition().links[0]
+            ? thePartition().links[0]   // ~ partition.Links[0] in C#
             : lionWebClient.forest.createNode(testLanguageBase.LinkTestConcept, id) as LinkTestConcept
 
-    const dataTypeTestConcept = () => thePartition().data!
+    const dataTypeTestConcept = () =>
+        thePartition().data!   // ~ partition.Data in C#
 
     return async (task: keyof typeof recognizedTasks, queryId: string) => {
         console.log(clientInfo(`client "${lionWebClient.clientId}" is executing task "${task}"`))
         switch (task) {
+
             case "SignOn":
                 return await lionWebClient.signOn(queryId, "myRepo")
+
             case "SubscribeToChangingPartitions":
                 return await lionWebClient.subscribeToChangingPartitions(queryId, {
                     creation: true,
                     deletion: true,
                     partitions: true
                 })
+
             case "SignOff":
                 return await lionWebClient.signOff(queryId)
-            case "Wait": {
+
+            case "Wait":
+                return waitForReceivedMessages(1)
+
+            case "AddPartition": {
+                const partition = lionWebClient.forest.createNode(testLanguageBase.TestPartition, "partition") as TestPartition
+                partition.data = lionWebClient.forest.createNode(testLanguageBase.DataTypeTestConcept, "data") as DataTypeTestConcept
+                partition.addLinks(lionWebClient.forest.createNode(testLanguageBase.LinkTestConcept, "link") as LinkTestConcept)
+                lionWebClient.addPartition(partition)
                 return waitForReceivedMessages(1)
             }
+
             case "AddStringValue_0_1":
                 dataTypeTestConcept().stringValue_0_1 = "new property"
                 return waitForReceivedMessages(1)
+
             case "SetStringValue_0_1":
                 dataTypeTestConcept().stringValue_0_1 = "changed property"
                 return waitForReceivedMessages(1)
+
             case "DeleteStringValue_0_1":
                 dataTypeTestConcept().stringValue_0_1 = undefined
                 return waitForReceivedMessages(1)
+
             case "AddName_Containment_0_1":
                 linkTestConcept().containment_0_1!.name = "my name"
                 return waitForReceivedMessages(1)
+
             case "AddAnnotation":
-                linkTestConcept().addAnnotation(annotation("annotation"))
+                thePartition().addAnnotation(annotation("annotation"))
                 return waitForReceivedMessages(1)
+
             case "AddAnnotations":
-                linkTestConcept().addAnnotation(annotation("annotation0"));   // (keep ;!)
-                linkTestConcept().addAnnotation(annotation("annotation1"))
+                thePartition().addAnnotation(annotation("annotation0"));   // (keep ;!)
+                thePartition().addAnnotation(annotation("annotation1"))
                 return waitForReceivedMessages(2)
+
             case "AddAnnotation_to_Containment_0_1":
                 linkTestConcept().containment_0_1!.addAnnotation(annotation("annotation"))
                 return waitForReceivedMessages(1)
-            case "DeleteAnnotation":
-                linkTestConcept().removeAnnotation(linkTestConcept().annotations[0])
-                return waitForReceivedMessages(1)
+
+            case "DeleteAnnotation": {
+                const nAnnotations = thePartition().annotations.length
+                thePartition().annotations.forEach((annotation) => {
+                    thePartition().removeAnnotation(annotation)
+                })
+                return waitForReceivedMessages(nAnnotations)
+            }
+
             case "MoveAnnotationInSameParent":
-                linkTestConcept().insertAnnotationAtIndex(lastOfArray(linkTestConcept().annotations), 0)
+                thePartition().insertAnnotationAtIndex(lastOfArray(thePartition().annotations), 0)
                 return waitForReceivedMessages(1)
+
             case "MoveAnnotationFromOtherParent":
-                linkTestConcept().addAnnotation(linkTestConcept().containment_0_1!.annotations[0])
+                thePartition().addAnnotation(linkTestConcept().containment_0_1!.annotations[0])
                 return waitForReceivedMessages(1)
+
             case "AddReference_0_1_to_Containment_0_1":
                 linkTestConcept().reference_0_1 = linkTestConcept().containment_0_1
                 return waitForReceivedMessages(1)
+
             case "AddReference_0_1_to_Containment_1":
                 linkTestConcept().reference_0_1 = linkTestConcept().containment_1
                 return waitForReceivedMessages(1)
+
             case "DeleteReference_0_1":
                 linkTestConcept().reference_0_1 = undefined
                 return waitForReceivedMessages(1)
+
             case "AddContainment_0_1":
                 linkTestConcept().containment_0_1 = linkTestConcept("containment_0_1")
                 return waitForReceivedMessages(1)
+
             case "AddContainment_1":
                 linkTestConcept().containment_1 = linkTestConcept("containment_1")
                 return waitForReceivedMessages(1)
+
             case "ReplaceContainment_0_1":
                 linkTestConcept().replaceContainment_0_1With(linkTestConcept("substitute"))
                 return waitForReceivedMessages(1)
+
             case "DeleteContainment_0_1":
                 linkTestConcept().containment_0_1 = undefined
                 return waitForReceivedMessages(1)
+
             case "AddContainment_0_1_Containment_0_1":
                 linkTestConcept().containment_0_1!.containment_0_1 = linkTestConcept("containment_0_1_containment_0_1")
                 return waitForReceivedMessages(1)
+
             case "AddContainment_1_Containment_0_1":
                 linkTestConcept().containment_1.containment_0_1 = linkTestConcept("containment_1_containment_0_1")
                 return waitForReceivedMessages(1)
+
             case "AddContainment_0_n":
                 linkTestConcept().addContainment_0_n(linkTestConcept("containment_0_n_child0"));   // (keep ;!)
                 linkTestConcept().addContainment_0_n(linkTestConcept("containment_0_n_child1"))
                 return waitForReceivedMessages(2)
+
             case "AddContainment_0_n_Containment_0_n": {
                 const outerChild = linkTestConcept("containment_0_n_child0")
                 outerChild.addContainment_0_n(linkTestConcept("containment_0_n_containment_0_n_child0"))
                 linkTestConcept().addContainment_0_n(outerChild)
                 return waitForReceivedMessages(1)
             }
+
             case "AddContainment_1_n":
                 linkTestConcept().addContainment_1_n(linkTestConcept("containment_1_n_child0"));   // (keep ;!)
                 linkTestConcept().addContainment_1_n(linkTestConcept("containment_1_n_child1"))
                 return waitForReceivedMessages(2)
+
             case "MoveAndReplaceChildFromOtherContainment_Single":
                 linkTestConcept().containment_1.replaceContainment_0_1With(linkTestConcept().containment_0_1!.containment_0_1!)
                 return waitForReceivedMessages(1)
+
             case "MoveAndReplaceChildFromOtherContainmentInSameParent_Single":
                 linkTestConcept().replaceContainment_1With(linkTestConcept().containment_0_1!)
                 return waitForReceivedMessages(1)
+
             case "MoveAndReplaceChildFromOtherContainment_Multiple":
                 if (linkTestConcept().containment_1_n.length === 0) {
                     throw new Error(`can't replace an item of an array with no items`)
@@ -217,35 +258,34 @@ export const taskExecutor = (lionWebClient: LionWebClient, semanticLogItems: ISe
                     linkTestConcept().containment_1_n.length - 1
                 )
                 return waitForReceivedMessages(1)
+
             case "MoveChildInSameContainment":
                 linkTestConcept().addContainment_0_nAtIndex(lastOfArray(linkTestConcept().containment_0_n), 0)
                 // Note: this is effectively a move rather than an insert — hence the name of the task.
                 return waitForReceivedMessages(1)
+
             case "MoveChildFromOtherContainment_Single":
                 linkTestConcept().containment_1 = linkTestConcept().containment_0_1!.containment_0_1!
                 return waitForReceivedMessages(1)
+
             case "MoveChildFromOtherContainment_Multiple":
                 linkTestConcept().addContainment_1_nAtIndex(lastOfArray(linkTestConcept().containment_0_n).containment_0_n[0], 1)
                 return waitForReceivedMessages(1)
+
             case "MoveChildFromOtherContainmentInSameParent_Single":
                 linkTestConcept().containment_1 = linkTestConcept().containment_0_1!
                 return waitForReceivedMessages(1)
+
             case "MoveChildFromOtherContainmentInSameParent_Multiple":
                 linkTestConcept().addContainment_1_nAtIndex(lastOfArray(linkTestConcept().containment_0_n), 1)
                 return waitForReceivedMessages(1)
-            case "AddPartition": {
-                const partition = lionWebClient.forest.createNode(testLanguageBase.TestPartition, "partition") as TestPartition
-                partition.data = lionWebClient.forest.createNode(testLanguageBase.DataTypeTestConcept, "data") as DataTypeTestConcept
-                partition.addLinks(lionWebClient.forest.createNode(testLanguageBase.LinkTestConcept, "link") as LinkTestConcept)
-                lionWebClient.addPartition(partition)
-                return waitForReceivedMessages(1)
-            }
 
             default: {
                 // (shouldn't happen because of upfront validation of tasks)
                 console.log(genericWarning(`task "${task}" is unknown => ignored`))
                 return Promise.resolve()
             }
+
         }
     }
 }
