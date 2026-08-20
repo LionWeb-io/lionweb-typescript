c8de93cf35c8e807f9c58d5eade41a0a8964435d
Meinte Boersma <meinte.boersma@gmail.com>
2026-08-20 10:43:32 +0200

    (re-organize/clean up class-core tests a bit)

--- packages/class-core-test/src/deltas/serialization.tests.ts
+++ packages/class-core-test/src/deltas/serialization.tests.ts
@@ -23,7 +23,7 @@ import {
 } from "@lionweb/class-core"
 
 import { attachedDataTypeTestConcept, TestEnumeration, TestLanguageBase } from "@lionweb/class-core-test-language"
-import { deepEqual, equal } from "./assertions.js"
+import { deepEqual, equal } from "../assertions.js"
 
 const testLanguageBase = TestLanguageBase.INSTANCE
 
